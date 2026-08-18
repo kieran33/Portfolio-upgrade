c446d4df734ea3c763a951cd9af5d6e0f5a56bca
kieran33 <lavole.kieran@gmail.com>
2026-08-18 08:56:43 +0200

    feat(backend): change url

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ if (contactForm) {
     const message = document.getElementById('message').value;
 
     try {
-      const response = await fetch('http://localhost:3000/api/contact', {
+      const response = await fetch('https://portfolio-upgrade-production.up.railway.app/api/contact', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name, email, message }),
